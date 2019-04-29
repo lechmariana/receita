fc7e7d72cdbfffe81d6185802aac0b8dc1902092
mjoselli <mejoselli@gmail.com>
2019-04-29 13:43:37 -0300

    Projeto inicial para começo da semana 2 do CC

---
+++ ReceitaApp - base/ReceitaApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ReceitaApp
+//
+//  Created by APPLE DEVELOPER ACADEMY on 23/04/19.
+//  Copyright © 2019 APPLE DEVELOPER ACADEMY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
